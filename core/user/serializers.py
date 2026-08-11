d5cca369c15d4cbf1799e30b855d7719314cb58a
gya1904 <anjuplus1156@gmail.com>
2026-08-11 18:00:30 +0900

    remove user_id from UserProfileSerializer response

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -4,6 +4,6 @@ from core.models import User
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['user_id', 'user_name', 'score']
+        fields = ['user_name', 'score']
         # Prevent users from modifying their own user_id or score directly
-        read_only_fields = ['user_id', 'score']
+        read_only_fields = ['score']
